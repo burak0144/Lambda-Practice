7a6a0aacd514f25d02b5cb4065926d9e85e2dd60
BURAK LAFCI <karanfilkaranfil822@gmail.com>
2022-09-04 23:01:27 +0300

    04.09.22

---
+++ Methodlarim.java
@@ -0,0 +1,13 @@
+package day52_lambdaPractice;
+
+public class Methodlarim {
+    public static boolean ciftElemaniBul(int x){
+        return x%2==0;
+    }
+    public static void yazdir(Object obj){
+        System.out.print(obj+" ");
+    }
+    public static void yazdir2(Object obj){
+        System.out.println(obj+" ");
+    }
+}
---
+++ Runner01.java
@@ -0,0 +1,27 @@
+package day52_lambdaPractice;
+
+public class Runner01 extends Lambda01{
+    public static void main(String[] args) {
+        aHarfleriSilenGerisiniYazdiran(names);
+        maxElemanBul(numbers);
+        elemanlariTopla(numbers);
+        elemanlariCarp(numbers);
+        teklerinKareBuyuktenKucuge(numbers);
+        System.out.println();
+        alfabetikBuyukHarfTekrarsiz(menu);
+        System.out.println();
+        sonHarfeGoreTersSira(menu);
+        System.out.println();
+        karakterSayiYedidenBuyuk(menu);
+        System.out.println();
+        xIleBitenEleman(menu);
+        karkterEnFazla(menu);
+        System.out.println();
+        sonHarfeGoreSiralaIlkHaricElmanlariYaz(menu);
+
+
+
+
+
+    }
+}
